3cda4a1c77872f9e72de5f910025df0867d78d6f
ITVash <vash_dn@mail.ru>
2019-09-16 22:43:18 +0300

    Fix

--- mapgorsvet/src/App.js
+++ mapgorsvet/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import GoogleMap from './Components/Google-map';
+import GoogleMaps from './Components/Google-map';
 
 function App() {
   return (
     <div className="App">
-      <GoogleMap></GoogleMap>
+      <GoogleMaps></GoogleMaps>
     </div>
   );
 }
--- mapgorsvet/src/Components/Google-map.js
+++ mapgorsvet/src/Components/Google-map.js
@@ -1,8 +1,23 @@
 import React from 'react';
-//import
+import {GoogleMap, withScriptjs, withGoogleMap} from 'react-google-maps';
 
-export default function GoogleMap() {
+ function Map() {
+   return (
+    <GoogleMap 
+      defaultZoom={12}
+      defaultCenter={{lat: 48.015875, lng: 37.801341}} />
+   );  
+ }
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+export default function GoogleMaps() {  
   return (
-    <div>Проверка)))))</div>
-  )
+    <div style={{width: '100%', height: '100vh'}}>
+      <WrappedMap 
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyBxslvjoPgptzBC6qj5KWr_SrAosWt63R8&v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement={<div style= {{height: '100%'}} />}
+        containerElement={<div style= {{height: '100%'}} />}
+        mapElement={<div style= {{height: '100%'}} />} />
+    </div>
+  );
 }
