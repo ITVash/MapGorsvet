89eb40a5b8c02ad6631247afd6cd4ade6ca34a73
ITVash <vash_dn@mail.ru>
2019-09-16 21:30:27 +0300

    Upp File

---
+++ mapgorsvet/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import GoogleMap from './Components/Google-map';
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleMap></GoogleMap>
+    </div>
+  );
+}
+
+export default App;
---
+++ mapgorsvet/src/Components/Google-map.js
@@ -0,0 +1,8 @@
+import React from 'react';
+//import
+
+export default function GoogleMap() {
+  return (
+    <div>Проверка)))))</div>
+  )
+}
